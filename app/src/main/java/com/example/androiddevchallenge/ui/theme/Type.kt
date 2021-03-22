35a3dc9b537022fe34754eb47176bc0a8ede2498
André Mion <andremion@gmail.com>
2021-03-22 22:51:36 +0000

    Update themes

--- app/src/main/java/com/example/androiddevchallenge/ui/theme/Type.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/theme/Type.kt
@@ -23,8 +23,8 @@ import androidx.compose.ui.unit.sp
 
 // Set of Material typography styles to start with
 val typography = Typography(
+    defaultFontFamily = FontFamily.SansSerif,
     body1 = TextStyle(
-        fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp
     )
